06e29dfaf615220175265f0deb4f88624de96577
N3t0n <n3t0n.dev@pm.me>
2026-03-31 08:31:27 +0200

    Exercise 1.11 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,7 +12,10 @@ const Button = ({text, onClick}) => {
 
 const StatisticLine = ({text, value}) => {
     return (
-      <p> {text} : {value} </p>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
     )
   }
 
@@ -28,15 +31,16 @@ const Statistics = ({good, neutral, bad}) => {
     )
   }else {
     return (
-      <div>
-        <StatisticLine text="Good" value={good}/>
-        <StatisticLine text="Neutral" value={neutral}/>
-        <StatisticLine text="Bad" value={bad}/>
-        <StatisticLine text="Total" value={total}/>
-        <StatisticLine text="Average" value={average}/>
-        <StatisticLine text="Positive" value={positivePercentage}/>
-
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good}/>
+          <StatisticLine text="Neutral" value={neutral}/>
+          <StatisticLine text="Bad" value={bad}/>
+          <StatisticLine text="Total" value={total}/>
+          <StatisticLine text="Average" value={average}/>
+          <StatisticLine text="Positive" value={positivePercentage}/>
+        </tbody>
+      </table>
     )
   
 }
@@ -69,6 +73,7 @@ const App = () => {
 
       <h2>Statistics</h2>
         <Statistics good={good} neutral={neutral} bad={bad} />
+     
     </div>
   )
 }
