62486cc4c3cd3c7ab23dc8caaf96091503138ce0
N3t0n <n3t0n.dev@pm.me>
2026-04-13 08:09:27 +0200

    Exercise 3.18 completed

--- part3/phonebook3/back-phonebook/index.js
+++ part3/phonebook3/back-phonebook/index.js
@@ -17,44 +17,21 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 app.get('/', (req, res) => {
     res.send('<h1>Testing Express</h1>')
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people
+app.get('/info', (req, res, next) => {
+    Person.countDocuments({}).then(count => {
+       res.send(`<p>Phonebook has info for ${count} people
         <br>
         <br>
         ${Date()}
         </p>`
     )
-    console.log(persons)
-    console.log(Date())
+    })
+    .catch(error => next(error))
+
 })
 
 app.get('/api/persons', (req, res) => {
