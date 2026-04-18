e929cb92e2bb65cee10abace15d5c8de561a1798
N3t0n <n3t0n.dev@pm.me>
2026-04-18 11:40:40 +0200

    Exercises 4.4 and 4.5 completed

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,26 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((fav, next) => {
+        return fav.likes > next.likes ? fav : next
+    })
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
