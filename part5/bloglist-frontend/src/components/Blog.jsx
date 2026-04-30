3c360d41990896d1242cea336c6d80c1c21106a2
N3t0n <n3t0n.dev@pm.me>
2026-04-30 13:05:50 +0200

    Exercise 5.7 completed

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const handleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      <div style={{ display: visible ? '' : 'none' }}>
+        {blog.url} <br />
+        {blog.likes} likes <button>like</button><br />
+        {blog.user.username}
+      </div>
+      <button onClick={handleVisibility}>
+        {visible ? 'hide' : 'view'}
+      </button>
+    </div>
+
+  )
+}
 
 export default Blog
